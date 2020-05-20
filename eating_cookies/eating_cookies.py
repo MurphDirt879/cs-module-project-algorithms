2109541e07fbff5fb9c8b4aafb188ebc9b50b998
MurphDirt879 <boxert879@hotmail.com>
2020-05-20 18:55:26 -0400

    Should be MVP ... if no ... oh well.

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,10 +2,14 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    # Your code here
-    return n
-    pass
+def eating_cookies(n, cache={1: 1}):
+    if n < 0:
+        return 0
+    if n == 0:
+        return 1
+    if n not in cache:
+        cache[n] = eating_cookies(n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
+    return cache[n]
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
